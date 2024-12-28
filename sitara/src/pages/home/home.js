85734fc536cdf0a0dcc16dfb68df4a9b8dda17aa
sahil <sahilkulkarni069@gmail.com>
2024-12-28 20:01:02 +0530

    practice changes

--- sitara/src/pages/home/home.js
+++ sitara/src/pages/home/home.js
@@ -3,8 +3,11 @@ function home() {
         <>
            <div className="container">
             <div className="row">
-                <div className="col-md-6 col-sm-12">hello</div>
-                <div className="col-md-6 col-sm-12">sitara</div>
+                <div className="col-md d-flex justify-content-center col-sm-12 ">hello</div>
+                <div className="col-md col-sm-12">sitara</div>
+                <div className="col-md col-sm-12">sitara</div>
+                <div className="col-md col-sm-12">sitara</div>
+                <div className="col-md col-sm-12">sitara</div>
             </div>
            </div>
         </>
